0ec089f76c842698c9ad6790f0a90eed507c7dbe
Anirudhh <anirudhh1988@gmail.com>
2016-07-06 20:41:51 +0530

    parse page insights

--- app/views/page.blade.php
+++ app/views/page.blade.php
@@ -6,19 +6,27 @@
 </head>
 <body>
 	<div>
-		<a href="/">Back To Pages</a>
-		@foreach($page_insight as $key => $value)
-			<h4>Title: <span>{{$value->title}}</span></h4>
-			<h5>Description: <span>{{$value->description}}</span></h5>
-				<?php 
-					$array = json_decode(json_encode($value->values), True);
-					for ($i=0; $i < count($array); $i++) { 
-						$innerArray = json_decode(json_encode($array[$i]), True);
-							var_dump($innerArray);
-					}
-				?>
-
-		@endforeach
+		@if($page_insight)
+			<a href="/">Back To Pages</a>
+			@foreach($page_insight as $key => $value)
+				<h4>Title: <span>{{$value->title}}</span></h4>
+				<h5>Description: <span>{{$value->description}}</span></h5>
+					<?php 
+						$array = json_decode(json_encode($value->values), True);
+						for ($i=0; $i < count($array); $i++) { 
+							$innerArray = json_decode(json_encode($array[$i]), True);
+							foreach ($innerArray as $key => $value) {
+								$value1 = serialize($value);
+								print "$key = $value1<br>";
+								print "<br>";
+							}
+						}
+					?>
+			@endforeach
+		@else
+			<h4>Page Insights Unavailable</h4>
+			<a href="/">Back To Pages</a>
+		@endif
 	</div>
 </body>
 </html>
